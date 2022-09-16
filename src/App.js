8476c5d1306841623b7401c0931afceadd79a51f
mhan <mananyan7@gmail.com>
2022-09-16 21:13:01 +0400

    add placheholders

--- src/App.js
+++ src/App.js
@@ -1,64 +1,80 @@
-import React, { useContext, useState } from "react";
-import { StoreContext } from "./index";
-import Modal from "./modal/Modal";
-import "./App.scss";
+import React, { useContext, useState } from 'react';
+import { StoreContext } from './index';
+import Modal from './modal/Modal';
+import './App.scss';
 
 function App(props) {
-    const store = useContext(StoreContext);
-    const [error, setError] = useState({ firstName: false, lastName: false });
-    const [show, setShow] = useState(false);
+  const store = useContext(StoreContext);
+  const [error, setError] = useState({
+    firstName: false,
+    lastName: false,
+  });
+  const [show, setShow] = useState(false);
 
-    const onChange = e => {
-        const name = e.target.name;
-        const value = e.target.value;
-        setError({ ...error, [name]: false });
-        store.update(value, name);
-    };
+  const onChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setError({ ...error, [name]: false });
+    store.update(value, name);
+  };
 
-    const onSubmit = e => {
-        e.preventDefault();
-        if (store.firstName && store.lastName) {
-            setShow(true);
-        } else {
-            setError({ firstName: !store.firstName, lastName: !store.lastName });
-        }
-    };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (store.firstName && store.lastName) {
+      setShow(true);
+    } else {
+      setError({
+        firstName: !store.firstName,
+        lastName: !store.lastName,
+      });
+    }
+  };
 
-    return (
-        <div className="App">
-            <form onSubmit={onSubmit}>
-                <div className="form-container">
-                    <div className="input-container">
-                        <input
-                            type="text"
-                            name="firstName"
-                            className={`input-name ${error.firstName && "error"}`}
-                            value={store.firstName}
-                            onChange={onChange}
-                        />
-                        {error.firstName && <div className="error-text">Field is Required</div>}
-                    </div>
-                    <div className="input-container">
-                        <input
-                            type="text"
-                            name="lastName"
-                            className={`input-name ${error.lastName && "error"}`}
-                            value={store.lastName}
-                            onChange={onChange}
-                        />
-                        {error.lastName && <div className="error-text">Field is Required</div>}
-                    </div>
+  return (
+    <div className="App">
+      <form onSubmit={onSubmit}>
+        <div className="form-container">
+          <div className="input-container">
+            <input
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              className={`input-name ${error.firstName && 'error'}`}
+              value={store.firstName}
+              onChange={onChange}
+            />
+            {error.firstName && (
+              <div className="error-text">Field is Required</div>
+            )}
+          </div>
+          <div className="input-container">
+            <input
+              type="text"
+              name="lastName"
+              placeholder="Last Name"
+              className={`input-name ${error.lastName && 'error'}`}
+              value={store.lastName}
+              onChange={onChange}
+            />
+            {error.lastName && (
+              <div className="error-text">Field is Required</div>
+            )}
+          </div>
 
-                    <button className="form-button" type="submit">
-                        Submit Data
-                    </button>
-                </div>
-            </form>
-            <Modal title="My Modal" onClose={() => setShow(false)} show={show}>
-                {store.firstName + " " + store.lastName}
-            </Modal>
+          <button className="form-button" type="submit">
+            Submit Data
+          </button>
         </div>
-    );
+      </form>
+      <Modal
+        title="My Modal"
+        onClose={() => setShow(false)}
+        show={show}
+      >
+        {store.firstName + ' ' + store.lastName}
+      </Modal>
+    </div>
+  );
 }
 
 export default App;
